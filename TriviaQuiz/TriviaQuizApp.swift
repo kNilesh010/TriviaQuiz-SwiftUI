200a9d50d9a10aee75193b9615c1280cea014f74
Nilesh Kumar <k.nilesh010@gmail.com>
2022-07-23 17:15:18 +0530

    Initial Commit

---
+++ TriviaQuiz/TriviaQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaQuizApp.swift
+//  TriviaQuiz
+//
+//  Created by Nilesh Kumar on 23/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
